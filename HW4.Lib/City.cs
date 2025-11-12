d5f68e91d2715ef68b5755dc344498cae05fa74f
ArturKrush <arturkrush@gmail.com>
2025-11-12 23:37:27 +0200

    Argument name was corrected in Employee, operators were overloaded in City and then tested in main()

---
+++ HW4.Lib/City.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    public class City
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (value == " " || value == "   ")
+                    throw new InvalidDataException("City must have a name.");
+                name = value;
+            }
+        }
+        private long population;
+        public long Population
+        {
+            get { return population; }
+            private set
+            {
+                if (value <= 0)
+                    throw new InvalidDataException("City population must be higher than 0.");
+                population = value;
+            }
+        }
+
+        public City(string name, long population)
+        {
+            Name = name;
+            Population = population;
+        }
+
+        public static City operator +(City city, long inc)
+        {
+            return new City(city.Name, city.Population + inc);
+        }
+
+        public static City operator -(City city, long dec)
+        {
+            return new City(city.Name, city.Population - dec);
+        }
+
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.Population == city2.Population;
+        }
+
+        public static bool operator !=(City city1, City city2)
+        {
+            return city1.Population != city2.Population;
+        }
+
+        public static bool operator >(City city1, City city2)
+        {
+            return city1.Population > city2.Population;
+        }
+
+        public static bool operator <(City city1, City city2)
+        {
+            return city1.Population < city2.Population;
+        }
+    }
+}
--- HW4/Program.cs
+++ HW4/Program.cs
@@ -32,6 +32,36 @@ namespace Homework4
                 Console.WriteLine("{0} salary was got higher compare to {1}", employee1AfterDec.LastName, employee2.LastName);
             else
                 Console.WriteLine("{0} salary was got higher compare to {1}", employee1AfterDec.LastName, employee2.LastName);
+            Console.WriteLine("-------------");
+
+            //Task 2
+            City city1 = new City("Odesa", 1000_000);
+            City city1AfterDec = city1 - 100_000;
+            Console.WriteLine("{0} population was decreased to {1}", city1AfterDec.Name, city1AfterDec.Population);
+            City city2 = new City("Lviv", 750_000);
+            City city2AfterInc = city2 + 125_000;
+            Console.WriteLine("{0} population was increased to {1}", city2AfterInc.Name, city2AfterInc.Population);
+
+            if(city1 == city1AfterDec)
+                Console.WriteLine("{0} population wasn't changed", city1AfterDec.Name);
+            else
+                Console.WriteLine("{0} population was changed", city1AfterDec.Name);
+
+            if (city2 != city2AfterInc)
+                Console.WriteLine("{0} population was changed", city2AfterInc.Name);
+            else
+                Console.WriteLine("{0} population wasn't changed", city2AfterInc.Name);
+
+            if (city1AfterDec > city2AfterInc)
+                Console.WriteLine("{0} is still more populated than {1}", city1AfterDec.Name, city2AfterInc.Name);
+            else
+                Console.WriteLine("{0} not more has a higher population than {1}", city1AfterDec.Name, city2AfterInc.Name);
+
+            if (city1AfterDec < city2AfterInc + 30_000)
+                Console.WriteLine("{0} could be towards {1} by population soon", city2AfterInc.Name, city1AfterDec.Name);
+            else
+                Console.WriteLine("{0} will not reach {1} population soon", city2AfterInc.Name, city1AfterDec.Name);
+            Console.WriteLine("-------------");
         }
     }
 }
