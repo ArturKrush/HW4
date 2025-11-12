10980a931dfad8e9c5dbf738c1b6589f259828e2
ArturKrush <arturkrush@gmail.com>
2025-11-12 22:55:37 +0200

    HW4.Lib project reference was added to HW4, operators were overloaded in Employee class and tested in main()

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -3,9 +3,35 @@ namespace Homework4
 {
     public class Program
     {
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //Task 1
+            Employee employee1 = new Employee("Statham", 500);
+            Employee employee1AfterInc = employee1 + 100;
+            Console.WriteLine(employee1AfterInc.Salary);
+            Employee employee1AfterDec = employee1AfterInc - 50.25;
+            Console.WriteLine(employee1AfterDec.Salary);
 
+            Employee employee2 = new Employee("Dwayne", 500);
+            if (employee1 == employee2)
+                Console.WriteLine("{0} and {1} salaries are equal", employee1.LastName, employee2.LastName);
+            else
+                Console.WriteLine("{0} and {1} salaries are not equal", employee1.LastName, employee2.LastName);
+
+            if (employee1AfterDec != employee2)
+                Console.WriteLine("After counting {0} and {1} salaries are not equal", employee1AfterDec.LastName, employee2.LastName);
+            else
+                Console.WriteLine("After counting {0} and {1} salaries are equal", employee1AfterDec.LastName, employee2.LastName);
+
+            if (employee1 > employee2)
+                Console.WriteLine("{0} salary have been higher compare to {1}", employee1.LastName, employee2.LastName);
+            else
+                Console.WriteLine("{0} salary haven't been higher compare to {1}", employee1.LastName, employee2.LastName);
+
+            if (employee2 < employee1AfterDec)
+                Console.WriteLine("{0} salary was got higher compare to {1}", employee1AfterDec.LastName, employee2.LastName);
+            else
+                Console.WriteLine("{0} salary was got higher compare to {1}", employee1AfterDec.LastName, employee2.LastName);
         }
     }
 }
